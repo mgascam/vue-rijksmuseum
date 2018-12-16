b32270766f554703c17744f423bdb2db9188a5e0
Miguel Gasca <miguelgasca@gmail.com>
2018-12-16 23:42:41 +0100

    Initial

---
+++ src/components/Api.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="api">
+        <figure>
+                <img :src="info.webImage.url" 
+                    width=80% alt="">
+                    <figcaption>{{  info.title }}</figcaption>
+        </figure>
+        <ul id="metadata" class="demo">
+            <li v-for="(value, key) in info" :key="key">
+                <b>{{key}}: </b>{{ value }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+const apiKey = 'ieTAe8ON';
+const apiUrl = `https://www.rijksmuseum.nl/api/en/collection/SK-C-5?key=${apiKey}&format=json`;
+export default {
+    name: 'Api',
+    data: function() {
+        return {
+            info: null
+        }
+    },
+    mounted() {
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => this.info = data.artObject);
+    }
+}
+</script>
+
+<style>
+    #metadata {
+        opacity: 0;
+    }
+    #metadata:hover {
+        opacity: 1;;
+    }
+</style>
